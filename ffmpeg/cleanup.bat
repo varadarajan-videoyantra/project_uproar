e717f6965c1a04869e786ce51f4301f6067d0889
Colosen <jith.var.1@gmail.com>
2024-06-04 16:26:40 +0530

    fixing errors

--- ffmpeg/cleanup.bat
+++ ffmpeg/cleanup.bat
@@ -5,8 +5,8 @@ del .\tmp\train\vocal-noise-rem-only.aac
 del .\tmp\train\vocal-only.aac
 del .\tmp\train\Video_Segment_train\*.mp4
 del .\tmp\serve\audio-input.aac
-del .\tmp\serve\vocal-noise-rem-only.aac
+del .\tmp\serve\serve-vocal-noise-rem-only.aac
 del .\tmp\serve\vocal-only.aac
-del .\tmp\serve\Video_Segment_train\*.mp4
+del .\tmp\serve\Video_Segment_serve\*.mp4
 
 
