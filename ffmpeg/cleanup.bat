461a11cc141c61e1d951af94dcb3494cb935dea7
Colosen <jith.var.1@gmail.com>
2024-06-04 15:58:47 +0530

    cleanup script added

---
+++ ffmpeg/cleanup.bat
@@ -0,0 +1,12 @@
+SET %WORK_DIR%=.
+del ..\prediction_serving.csv
+del .\tmp\train\audio-input.aac
+del .\tmp\train\vocal-noise-rem-only.aac
+del .\tmp\train\vocal-only.aac
+del .\tmp\train\Video_Segment_train\*.mp4
+del .\tmp\serve\audio-input.aac
+del .\tmp\serve\vocal-noise-rem-only.aac
+del .\tmp\serve\vocal-only.aac
+del .\tmp\serve\Video_Segment_train\*.mp4
+
+
